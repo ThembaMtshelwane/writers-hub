f28e9e86dc13ba06f84076e750b9a0718fe5fb78
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-10-25 16:26:26 +0200

    Landing Page

--- src/components/MainCardGrid.tsx
+++ src/components/MainCardGrid.tsx
@@ -1,10 +1,39 @@
 import MainCard from "./MainCard";
-import maincard from "../maincard.json";
+// import maincard from "../maincard.json";
+import { useEffect, useState } from "react";
+import { User } from "../types";
+import axios from "axios";
 
 const MainCardGrid = () => {
+  const [content, setContent] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchContent = async () => {
+      try {
+        const { data } = await axios.get("/api/users");
+        setContent(data);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    };
+    fetchContent();
+  }, []);
+
   return (
     <div className="contentCardGrid">
-      {maincard.map((book) => {
+      {content.map((con) => {
+        return con.content.map((cont, index) => (
+          <MainCard
+            key={index}
+            title={cont.title}
+            author={con.first_name + " " + con.last_name}
+            image={cont.image}
+            desc={cont.description}
+          />
+        ));
+      })}
+
+      {/* {maincard.map((book) => {
         return (
           <MainCard
             key={book.id}
@@ -14,7 +43,7 @@ const MainCardGrid = () => {
             desc={book.desc}
           />
         );
-      })}
+      })} */}
     </div>
   );
 };
--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,15 +1,48 @@
+import { useState } from "react";
 import HeroSlider from "../components/HeroSlider";
 import MainCardGrid from "../components/MainCardGrid";
 import MainSideBar from "../components/MainSideBar";
 import Navbar from "../components/Navbar";
+// import Button from "../components/Button";
 
 const LandingPage = () => {
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  console.log(searchQuery);
   return (
     <>
       <Navbar />
       <HeroSlider />
       <main className="grid grid-cols-[70%_30%] mt-10 gap-5 justify-self-center w-[95%] ">
-        <section className=" w-full">
+        <section className=" w-full space-y-8">
+          <div className="space-x-8">
+            {["Latest", "Poems", "Short Stories", "Long-Form"].map(
+              (types, index) => {
+                return (
+                  <a
+                    key={index}
+                    onClick={() => console.log("Link Clicked")}
+                    className="py-3 px-5 text-lg rounded-full min-w-[80px] bg-secondary text-white cursor-pointer border border-transparent hover:bg-white hover:border-secondary hover:text-black"
+                  >
+                    {types}
+                  </a>
+                  // <Button
+                  //   key={index}
+                  //   name={types}
+                  //   buttonFunction={() => setSearchQuery("Checking")}
+                  // />
+                );
+              }
+            )}
+          </div>
+
+          <input
+            type="text"
+            placeholder="Search"
+            id="search"
+            className="w-full rounded-full border border-slate-800 bg-transparent px-6 py-2 text-sm text-slate-800 focus:border-secondary focus:outline-none md:text-base "
+            onChange={(e) => setSearchQuery(e.target.value)}
+          />
           <MainCardGrid />
         </section>
         <MainSideBar />
