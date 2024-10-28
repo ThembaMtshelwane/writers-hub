0460ac93adc9a0191cfc69652cc70a2855035b93
ThembaMtshelwane <thembamm3@gmail.com>
2024-10-28 19:59:22 +0200

    Review section done

---
+++ src/components/ReviewSection.tsx
@@ -0,0 +1,21 @@
+import RubricInput from "./RubricInput";
+
+type ReviewSectionProps = {
+  isOpen: boolean;
+  setIsOpen: (isOpen: boolean) => void;
+  title: string;
+};
+
+const ReviewSection = ({ isOpen, setIsOpen, title }: ReviewSectionProps) => {
+  return (
+    <>
+      {isOpen && (
+        <section className="bg-black bg-opacity-20 h-screen fixed inset-0 top-0 flex items-center justify-center">
+          <RubricInput work={title} isOpen={isOpen} setIsOpen={setIsOpen} />
+        </section>
+      )}
+    </>
+  );
+};
+
+export default ReviewSection;
