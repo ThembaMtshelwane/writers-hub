32f390f1df09c048b152bcfa130331a383a5859f
Hilton-J <140448818+Hilton-J@users.noreply.github.com>
2024-10-30 21:12:38 +0200

    Router updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,70 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
-import LandingPage from "./pages/LandingPage";
-import ProfileLayout from "./layout/ProfileLayout";
-import PersonalInfoList from "./pages/profilePage/PersonalInfoList";
-import CreatorCardGrid from "./components/CreatorCardGrid";
-import NotFound from "./pages/NotFound";
-import Livefeed from "./pages/feedPage/Livefeed";
-import SinglePost from "./pages/SinglePost";
-import AuthLayout from "./layout/AuthLayout";
-import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import SplashPage from "./pages/SplashPage";
-import PreferencePage from "./pages/PreferencePage";
-import ShareWork from "./components/ShareWork";
-import ViewScore from "./components/ViewScore";
+import Signup from "./pages/Signup";
+import NotFound from "./pages/NotFound";
 import Writing from "./components/Writing";
 import Preview from "./components/Preview";
+import SinglePost from "./pages/SinglePost";
+import SplashPage from "./pages/SplashPage";
+import MainLayout from "./layout/MainLayout";
+import AuthLayout from "./layout/AuthLayout";
+import LandingPage from "./pages/LandingPage";
 import ReviewdPage from "./pages/ReviewdPage";
-// import { PostContext } from "./context/PostContext";
+import ShareWork from "./components/ShareWork";
+import ViewScore from "./components/ViewScore";
+import Livefeed from "./pages/feedPage/Livefeed";
+import ProfileLayout from "./layout/ProfileLayout";
+import PreferencePage from "./pages/PreferencePage";
 import { UserProvider } from "./context/UserProvider";
+import CreatorCardGrid from "./components/CreatorCardGrid";
+import PersonalInfoList from "./pages/profilePage/PersonalInfoList";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-export default function App() {
+const App = () => {
+  const router = createBrowserRouter([
+    { path: "*", element: <NotFound /> },
+    { path: "/", element: <SplashPage /> },
+    { path: "/index", element: <LandingPage /> },
+    { path: "/pref", element: <PreferencePage /> },
+    {
+      path: "/auth",
+      element: <AuthLayout />,
+      children: [
+        { path: "login", element: <Login /> },
+        { path: "signup", element: <Signup /> },
+      ],
+    },
+    {
+      path: "/feed",
+      element: <MainLayout />,
+      children: [{ index: true, element: <Livefeed /> }],
+    },
+    {
+      path: "/post",
+      element: <MainLayout />,
+      children: [
+        { index: true, element: <ShareWork /> },
+        { path: "write", element: <Writing /> },
+        { path: "preview", element: <Preview /> },
+        { path: ":username/:id", element: <SinglePost /> },
+      ],
+    },
+    {
+      path: "/profile",
+      element: <ProfileLayout />,
+      children: [
+        { path: "reviews", element: <ReviewdPage /> },
+        { path: "view-score", element: <ViewScore /> },
+        { path: "works", element: <CreatorCardGrid /> },
+        { path: "info/:userId", element: <PersonalInfoList /> },
+      ],
+    },
+  ]);
+
   return (
     <UserProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/auth" element={<AuthLayout />}>
-            <Route path="signup" element={<Signup />} />
-            <Route path="login" element={<Login />} />
-          </Route>
-
-          <Route path="/" element={<SplashPage />} />
-          <Route path="/index" element={<LandingPage />} />
-
-          <Route path="/pref" element={<PreferencePage />} />
-          <Route path="/feed" element={<MainLayout />}>
-            <Route index element={<Livefeed />} />
-          </Route>
-
-          <Route path="/post" element={<MainLayout />}>
-            <Route index element={<ShareWork />} />
-            <Route path="write" element={<Writing />} />
-            <Route path="preview" element={<Preview />} />
-            <Route path=":username/:id" element={<SinglePost />} />
-          </Route>
-
-          <Route path="/profile" element={<ProfileLayout />}>
-            <Route path="info/:userId" element={<PersonalInfoList />} />
-            <Route path="works" element={<CreatorCardGrid />} />
-            <Route path="reviews" element={<ReviewdPage />} />
-            <Route path="view-score" element={<ViewScore />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </UserProvider>
   );
-}
+};
+
+export default App;
