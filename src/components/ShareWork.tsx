bb47c296315f5ec4566b3f556bed23b5ccbb928c
fanelob46 <fanelob46@gmail.com>
2024-10-31 17:17:37 +0200

    responsiveness of the post page

--- src/components/ShareWork.tsx
+++ src/components/ShareWork.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate
 import Button from "./Button";
 import ReactQuill from "react-quill";
@@ -19,7 +19,7 @@ const ShareWork = () => {
   const [Description, setDescription] = useState("");
   const [base64, setBase64] = useState<string>("");
   const [Type, setType] = useState("");
-
+ const textRef = useRef<ReactQuill>(null);
   const navigate = useNavigate(); // Initialize navigate
 
   useEffect(() => {
@@ -48,12 +48,17 @@ const ShareWork = () => {
   };
 
   const handleNext = () => {
+
+
+    const qui = textRef.current?.getEditor();
+    const text = qui?.getText();
+
     // Object containing all the form data
     const formData = {
       Title,
       selectedGenre,
       Type,
-      Description,
+      Description : text,
       image,
       base64,
     };
@@ -66,15 +71,15 @@ const ShareWork = () => {
 
   return (
     <section className="py-3 px-3">
-      <div className="bg-white rounded-3xl grid px-4 xl:px-6 md:h-[600px] md:w-[450px] w-[260px] lg:w-[600px] pc:w-[1200px] pc:h-[750px] xl:w-[850px]">
-        <h1 className="pt-10 text-sm xl:text-xl">SHARE YOUR WORK</h1>
-        <form className="grid grid-cols-1 py-10 2xl:py-5 space-y-3 md:grid-cols-2 pc:px-10">
+      <div className="bg-white rounded-3xl grid px-4 xl:px-6  md:h-[840px] 2xl:h-[600px] md:w-[450px] w-[260px] lg:w-[600px] pc:w-[1200px] pc:h-[750px] xl:w-[850px]">
+        <h1 className="pt-10 text-sm xl:text-xl md:pt-3">SHARE YOUR WORK</h1>
+        <form className="grid grid-cols-1 py-10 2xl:py-5 space-y-3 md:grid-cols-1 pc:px-10 md:space-y-5 lg:grid-cols-2 md:place-content-center md:place-items-center ">
           <div className="flex flex-col ">
             <label htmlFor="file-upload">
               <img
                 src={image || "https://via.placeholder.com/300x200"}
                 alt="Upload Placeholder"
-                className="w-48 h-[100px] object-cover text-center bg-gray-400 cursor-pointer md:h-[490px] lg:w-[250px]  xl:w-[350px] pc:h-[540px] pc:w-[500px] "
+                className="w-48 h-[100px] object-cover text-center bg-gray-400 cursor-pointer md:h-[230px] md:w-[300px] lg:w-[250px] lg:h-[430px]  xl:w-[350px] pc:h-[540px] pc:w-[500px] 2xl:h-[460px]"
               />
             </label>
             <input
@@ -85,7 +90,7 @@ const ShareWork = () => {
               className="hidden"
             />
           </div>
-          <div className="border border-black w-[230px] md:w-[220px] md:px-2 md:h-[480px] px-3 pt-5 lg:w-[290px] lg:px-3 xl:w-[400px] xl:px-5    2xl:w-[400px] 2xl:h-[400px]  2xl:px-10 pc:h-[530px] pc:w-[450px] pc:px-6">
+          <div className="border border-black w-[230px] md:w-[300px]  md:px-5 md:h-[430px] md:text-sm px-3 pt-5 lg:w-[290px] lg:px-3 xl:w-[400px] xl:px-5    2xl:w-[400px] 2xl:h-[440px]  2xl:px-10 pc:h-[530px] pc:w-[450px] pc:px-6">
             <div className="mb-4">
               <label className="block text-gray-700 font-bold mb-2">
                 Title
@@ -94,7 +99,7 @@ const ShareWork = () => {
                 type="text"
                 id="title"
                 name="title"
-                className="border rounded-2xl w-[200px] lg:w-[270px] pc:w-[400px] xl:w-[350px] py-2 px-3 mb-2 kobus:w-[700px] border-black 2xl:w-[300px] md:w-[180px]"
+                className="border rounded-2xl w-[200px] lg:w-[270px] pc:w-[400px] xl:w-[350px]  py-2 px-3 mb-2 kobus:w-[700px] border-black 2xl:w-[300px] md:w-[250px]"
                 placeholder=""
                 required
                 value={Title}
@@ -108,7 +113,7 @@ const ShareWork = () => {
               <select
                 id="genres"
                 name="genres"
-                className="border rounded-2xl w-[200px] pc:w-[400px] lg:w-[270px] xl:w-[350px] py-2 px-3 mb-2 kobus:w-[700px] border-black 2xl:w-[300px] md:w-[180px]"
+                className="border rounded-2xl w-[200px] pc:w-[400px] lg:w-[270px] xl:w-[350px] py-2 px-3 mb-2 kobus:w-[700px] border-black 2xl:w-[300px] md:w-[250px]"
                 required
                 // value={Genres.id}
                 // onChange={(e) => setGenres(e.target.value)}
@@ -124,9 +129,9 @@ const ShareWork = () => {
               </select>
             </div>
 
-            <div className=" pb-6 space-y-2">
+            <div className=" pb-6 space-y-2 ">
               <div>
-                <h1>Type</h1>
+                <h1 className="text-sm">Type</h1>
               </div>
               <div className="flex">
                 <div className="flex items-center me-4 ">
@@ -141,7 +146,7 @@ const ShareWork = () => {
                   />
                   <label
                     htmlFor="movie-radio"
-                    className="ms-2 text-sm font-medium text-black kobus:text-3xl big:text-5xl"
+                    className="ms-2 text-sm font-medium text-black kobus:text-3xl big:text-5xl "
                   >
                     Poem
                   </label>
@@ -189,8 +194,9 @@ const ShareWork = () => {
               >
                 Description
               </label>
-              <div className="border border-black w-[200px] lg:w-[270px] pc:w-[400px] pc:h-[180px] xl:w-[350px] h-52 kobus:w-[700px] 2xl:w-[300px] 2xl:h-[130px] md:w-[180px] md:h-[120px]">
+              <div className="border border-black w-[200px] lg:w-[270px] pc:w-[400px] pc:h-[180px] xl:w-[350px] h-52 kobus:w-[700px] 2xl:w-[300px] 2xl:h-[130px] md:w-[250px] md:h-[100px]">
                 <ReactQuill
+                  ref={textRef}
                   modules={modules}
                   theme="snow"
                   value={Description}
@@ -202,7 +208,7 @@ const ShareWork = () => {
           </div>
         </form>
 
-        <div className="pb-3 md:flex md:justify-center">
+        <div className="pb-3 md:flex md:justify-center ">
           <Button name="Next" buttonFunction={handleNext} />
         </div>
       </div>
